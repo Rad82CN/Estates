30ca5a96676f9b65f656f31cb99d008fe54bf419
rad_cn <rad.bakhtavar82@gmail.com>
2024-07-10 19:16:07 +0430

    created auth routes for logging in/out and registering and created its own controller and functions

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register() {
+        return view('auth.register');
+    }
+
+    public function store() {
+        $validated = request()->validate(
+            [
+                'name' => 'required|min:3|max:20',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|confirmed|min:8',
+            ]
+        );
+
+        User::create(
+            [
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]
+        );
+
+        return redirect()->route('index');
+    }
+
+    public function login() {
+        return view('auth.login');
+    }
+
+    public function authenticate() {
+        $validated = request()->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required|min:8',
+            ]
+        );
+
+        if(auth()->attempt($validated)) {
+
+            request()->session()->regenerate();
+            
+            return redirect()->route('index')->with('success', 'Logged in successfully!');
+        }
+
+        return redirect()->route('index')->withErrors([
+            'email' => 'This email address is not valid',
+        ]);
+    }
+
+    public function logout() {
+        auth()->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('index');
+    }
+}
