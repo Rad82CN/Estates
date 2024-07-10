2619863317d3c81d4be53ae44bb4027e996e65f1
rad_cn <rad.bakhtavar82@gmail.com>
2024-07-10 18:41:28 +0430

    updated users migration and added admin role to it and updated left side bar blade file

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,28 +2,16 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="nav-link text-dark" href="#">
+                <a class="{{ (Route::is('index')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ route('index') }}">
                     <span>Home</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Explore</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Feed</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="{{ (Route::is('terms')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ route('terms') }}">
                     <span>Terms</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="{{ (Route::is('support')) ? 'text-white bg-primary rounded' : '' }} nav-link" href="{{ route('support') }}">
                     <span>Support</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                    <span>Settings</span></a>
             </li>
         </ul>
     </div>
